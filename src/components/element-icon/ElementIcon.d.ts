5c18111aa758538c722771c3a114ba55bef06c8c
XTR\aditya.leksono <aditya.leksono@xtremax.com>
2023-09-07 22:12:42 +0700

    add element icon component

---
+++ src/components/element-icon/ElementIcon.d.ts
@@ -0,0 +1,9 @@
+export type ElementType =
+  | 'grass'
+  | 'poison'
+  | 'fire'
+  | 'water'
+  | 'bug'
+  | 'normal'
+  | 'electric'
+  | 'ground';
---
+++ src/components/element-icon/ElementIcon.tsx
@@ -0,0 +1,25 @@
+import GrassIcon from '@mui/icons-material/Grass';
+import ScienceIcon from '@mui/icons-material/Science';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import PublicIcon from '@mui/icons-material/Public';
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import ContrastIcon from '@mui/icons-material/Contrast';
+import BugReportIcon from '@mui/icons-material/BugReport';
+
+import { ElementType } from './ElementIcon';
+
+export default function ElementIcon({ type }: { type: ElementType }) {
+  const typeToIconMapper: Record<ElementType, JSX.Element> = {
+    grass: <GrassIcon className="text-green"></GrassIcon>,
+    poison: <ScienceIcon className="text-purple"></ScienceIcon>,
+    fire: <WhatshotIcon className="text-red"></WhatshotIcon>,
+    water: <WaterDropIcon className="text-blue"></WaterDropIcon>,
+    bug: <BugReportIcon className="text-bug"></BugReportIcon>,
+    normal: <ContrastIcon className="text-grey"></ContrastIcon>,
+    electric: <ElectricBoltIcon className="text-yellow"></ElectricBoltIcon>,
+    ground: <PublicIcon className="text-brown"></PublicIcon>
+  };
+
+  return typeToIconMapper[type];
+}
