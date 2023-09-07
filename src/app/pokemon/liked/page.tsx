721499ab4ec9d6174713bd20f19ab0a7e2bc0316
XTR\aditya.leksono <aditya.leksono@xtremax.com>
2023-09-08 01:39:22 +0700

    apply liked pokemon

--- src/app/pokemon/liked/page.tsx
+++ src/app/pokemon/liked/page.tsx
@@ -1,7 +1,53 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+import Card from '../../../components/card/Card';
+import { useLikedPokemons } from './../../../context/LikedPokemonContext';
+
 export default function LikedPokemonPage() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const { likedPokemons } = useLikedPokemons();
+
+  useEffect(() => {
+    async function populateData() {
+      let mergedPokemonList = [...pokemonList];
+      for (let index = 0; index < likedPokemons.length; index++) {
+        const element = likedPokemons[index];
+        const response = await fetch(`/api/pokemon/${element}`);
+        const data = await response.json();
+        let result = {
+          name: '',
+          image: ''
+        };
+
+        result.image = data.sprites?.front_default;
+        result.name = data.name;
+
+        mergedPokemonList = [...mergedPokemonList, result];
+      }
+
+      setPokemonList(mergedPokemonList);
+    }
+
+    populateData();
+  }, []);
+
   return (
     <>
-      <h1>Liked Pokemon List</h1>
+      <div className="flex justify-between items-center mb-7">
+        <h1 className="uppercase font-bold text-3xl">Liked Pokemon</h1>
+
+        <Button>
+          <Link href={'/'}>Back</Link>
+        </Button>
+      </div>
+      {pokemonList && pokemonList.length > 0 ? (
+        <Card items={pokemonList} />
+      ) : (
+        'You havent added any liked Pokemon yet'
+      )}
     </>
   );
 }
