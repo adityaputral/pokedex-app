386666a88eaa10b520b6e90e82f894744b7cfa50
XTR\aditya.leksono <aditya.leksono@xtremax.com>
2023-09-07 16:01:48 +0700

    create basic pokemon detail page

--- src/app/pokemon/[pokemonName]/page.tsx
+++ src/app/pokemon/[pokemonName]/page.tsx
@@ -1,7 +1,74 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import { IPokemonDetail } from '@/types/PokemonData';
+
 export default function PokemonDetail() {
+  const params = useParams();
+  const [pokemonDetail, setPokemonDetail] = useState<IPokemonDetail>([]);
+
+  useEffect(() => {
+    async function populateData() {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${params.pokemonName}`
+      );
+      const data = await response.json();
+
+      setPokemonDetail(data);
+    }
+
+    populateData();
+  }, []);
+
   return (
     <>
       <h1>Pokemon Detail Page</h1>
+      <section>
+        <strong>Name</strong> : <p>{pokemonDetail.name || '-'}</p>
+        <strong>Height</strong> : <p>{pokemonDetail.height || '-'}</p>
+        <strong>Weight</strong> : <p>{pokemonDetail.weight || '-'}</p>
+        <strong>Types</strong> :{' '}
+        <p>
+          {pokemonDetail.types && pokemonDetail.types.length > 0
+            ? pokemonDetail.types.map(
+                (
+                  type: {
+                    type: {
+                      name: string;
+                    };
+                  },
+                  i: number
+                ) => (
+                  <>
+                    <p>{type.type.name}</p>
+                  </>
+                )
+              )
+            : '-'}
+        </p>
+        <strong>Stats</strong> :{' '}
+        <p>
+          {pokemonDetail.stats && pokemonDetail.stats.length > 0
+            ? pokemonDetail.stats.map(
+                (
+                  stat: {
+                    base_stat: number;
+                    stat: {
+                      name: string;
+                    };
+                  },
+                  i: number
+                ) => (
+                  <>
+                    <p>{stat.base_stat}</p>
+                    <p>{stat.stat.name}</p>
+                  </>
+                )
+              )
+            : '-'}
+        </p>
+      </section>
     </>
   );
 }
--- src/types/PokemonData.d.ts
+++ src/types/PokemonData.d.ts
@@ -5,3 +5,30 @@ export interface IPokemon {
 }
 
 export type Pokemons = IPokemon[];
+
+export interface IPokemonDetail {
+  name: string;
+  species: {
+    name: string;
+  };
+  moves: {
+    move: {
+      name: string;
+    };
+  }[];
+  stats: {
+    base_stat: number | string;
+    effort: number | string;
+    stat: {
+      name: string;
+    };
+  }[];
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  weight: number;
+  height: number;
+  sprites: Record<string, string | null>;
+}
