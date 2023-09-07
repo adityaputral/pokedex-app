6ade93c8eddad706acc9ac357d15d657effe001f
XTR\aditya.leksono <aditya.leksono@xtremax.com>
2023-09-07 16:01:41 +0700

    create basic pokemon listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,39 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { Pokemons, IPokemon } from '@/types/PokemonData';
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Pokemon Listing</h1>;
+  const [pokemonList, setPokemonList] = useState<Pokemons>([]);
+
+  useEffect(() => {
+    async function populateData() {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const data = await response.json();
+
+      setPokemonList(data.results);
+    }
+
+    populateData();
+  }, []);
+
+  return (
+    <>
+      <h1>Pokemon Listing</h1>
+
+      <section>
+        {pokemonList && pokemonList.length > 0
+          ? pokemonList.map((pokemon: IPokemon, i: number) => (
+              <>
+                <Link href={`/pokemon/${pokemon.name}`} key={i}>
+                  {pokemon.name}
+                </Link>
+                <br />
+              </>
+            ))
+          : 'No pokemon available'}
+      </section>
+    </>
+  );
 }
---
+++ src/types/PokemonData.d.ts
@@ -0,0 +1,7 @@
+export interface IPokemon {
+  name: string;
+  url: string;
+  img: string;
+}
+
+export type Pokemons = IPokemon[];
