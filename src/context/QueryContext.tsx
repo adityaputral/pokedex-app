9a31f04490fa298b1fd65794df94d455a7249966
XTR\aditya.leksono <aditya.leksono@xtremax.com>
2023-09-07 16:32:48 +0700

    rearrange query context position

---
+++ src/context/QueryContext.tsx
@@ -0,0 +1,25 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery
+} from '@tanstack/react-query';
+import { createContext, useContext, useState, forwardRef } from 'react';
+
+const QueryContext = createContext<{
+  isLoading: boolean;
+  error: unknown;
+  data: Record<string, any>;
+} | null>(null);
+
+const queryClient = new QueryClient();
+export function QueryProvider({ children }) {
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
+
+export function useQueryContext() {
+  const context = useContext(QueryContext);
+  if (!context) {
+    throw new Error('useSnackbar must be used within SnackbarProvider');
+  }
+  return context;
+}
